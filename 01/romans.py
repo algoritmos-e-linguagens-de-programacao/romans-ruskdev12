9de58d2e9be6b4f87e95b835792db799cff094e6
ruskdev12 <165857317+ruskdev12@users.noreply.github.com>
2024-06-08 16:06:14 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,8 +1,23 @@
 def int_to_roman(num):
-    # Implemente sua função aqui
-    pass
+   val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    roman_num = ''
+    i = 0
+    while  num > 0:
+        for _ in range(num // val[i]):
+            roman_num += syb[i]
+            num -= val[i]
+        i += 1
+    return roman_num
 
 
 def roman_to_int(s):
-    # Implemente sua função aqui
-    pass
+    roman_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_val = 0
+    for i in range(len(s)):
+        if i > 0 and roman_val[s[i]] > roman_val[s[i - 1]]:
+            int_val += roman_val[s[i]] - 2 * roman_val[s[i - 1]]
+        else:
+            int_val += roman_val[s[i]]
+    return int_val
+   
