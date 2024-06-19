733cc5e6e505e404ee9e797fc2d1acd510631896
ruskdev12 <165857317+ruskdev12@users.noreply.github.com>
2024-06-18 23:59:02 -0300

    Update2romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,15 +1,14 @@
 def int_to_roman(num):
-   val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     roman_num = ''
     i = 0
-    while  num > 0:
+    while num > 0:
         for _ in range(num // val[i]):
             roman_num += syb[i]
             num -= val[i]
         i += 1
     return roman_num
-
 
 def roman_to_int(s):
     roman_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -20,4 +19,3 @@ def roman_to_int(s):
         else:
             int_val += roman_val[s[i]]
     return int_val
-   
