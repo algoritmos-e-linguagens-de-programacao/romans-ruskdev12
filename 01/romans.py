c93d2c448b1ee1f9aacbb061427c71d4b99cd6fa
ruskdev12 <165857317+ruskdev12@users.noreply.github.com>
2024-06-20 00:25:44 -0300

    UpdateRomans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,21 +1,73 @@
 def int_to_roman(num):
-    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-    roman_num = ''
-    i = 0
-    while num > 0:
-        for _ in range(num // val[i]):
-            roman_num += syb[i]
-            num -= val[i]
-        i += 1
-    return roman_num
+    sub = num
+    resultado = ""
+
+    while sub != 0:
+        if sub == 4:
+            resultado += "IV"
+            sub -= 4
+        elif sub == 9:
+            resultado += "IX"
+            sub -= 9
+        elif sub >= 40 and sub < 50:
+            resultado += "XL"
+            sub -= 40
+        elif sub >= 90 and sub < 100:
+            resultado += "XC"
+            sub -= 90
+        elif sub >= 400 and sub < 500:
+            resultado += "CD"
+            sub -= 400
+        elif sub >= 900 and sub < 1000:
+            resultado += "CM"
+            sub -= 900
+        elif sub == 1:
+            resultado += "I"
+            sub -= 1
+        elif sub < 5:
+            resultado += "I"
+            sub -= 1
+        elif sub < 10:
+            resultado += "V"
+            sub -= 5
+        elif sub < 50:
+            resultado += "X"
+            sub -= 10
+        elif sub < 100:
+            resultado += "L"
+            sub -= 50
+        elif sub < 500:
+            resultado += "C"
+            sub -= 100
+        elif sub < 1000:
+            resultado += "D"
+            sub -= 500
+        elif sub >= 1000:
+            resultado += "M"
+            sub -= 1000
+            
+    return resultado
 
 def roman_to_int(s):
-    roman_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    int_val = 0
-    for i in range(len(s)):
-        if i > 0 and roman_val[s[i]] > roman_val[s[i - 1]]:
-            int_val += roman_val[s[i]] - 2 * roman_val[s[i - 1]]
+    roman_to_value = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(s):
+        current_value = roman_to_value[char]
+        if current_value < prev_value:
+            total -= current_value
         else:
-            int_val += roman_val[s[i]]
-    return int_val
+            total += current_value
+        prev_value = current_value
+
+    return total
